3a363dc8cc68d9da672c4842a6c5ce5bd32d57e6
Santiago Guzman <guzmansantiago833@gmail.com>
2025-11-15 12:54:50 -0500

    feat: add new features

--- backend/controllers/auth-controller.ts
+++ backend/controllers/auth-controller.ts
@@ -1,27 +1,35 @@
 import { Auth } from "../db/models";
-import * as crypto from "crypto";
+// import * as crypto from "crypto";
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt'
 
 const SECRET = process.env.SECRET;
 
-// Funcion para crear un hash sha256 y proteger contraseñas
-function getSHA256ofString(text: string) {
-	return crypto // Retornamos a crypto
-    .createHash("sha256") // Que crea un hash sha256
-    .update(text) // Que actualiza el texto a sha256
-    .digest("hex"); // Y resuelve con el sha256 en hexadecimal
+async function getBcryptHash(text: string){
+    const passHashed = await bcrypt.hash(text, 10);
+    return passHashed;
 }
+
+// // Funcion para crear un hash sha256 y proteger contraseñas
+// function getSHA256ofString(text: string) {
+// 	return crypto // Retornamos a crypto
+//     .createHash("sha256") // Que crea un hash sha256
+//     .update(text) // Que actualiza el texto a sha256
+//     .digest("hex"); // Y resuelve con el sha256 en hexadecimal
+// }
 
 // Creamos la funcion para autorizar un registro (recibimos email, password y userId)
 async function authRegister(email: string, password: string, userId: number) {
 	try {
+        const passHashed = await getBcryptHash(password);
+
         // Destructuramos en un array lo que nos de el findOrCreate (1. El elemento creado 2. El estado de creacion)
 		const [auth, authCreated] = await Auth.findOrCreate({
             where: { userId }, // Buscamos en auth el userId
             defaults: { // Si no existe, lo creamos con las propiedades recibidas
                 userId,
                 email,
-                password: getSHA256ofString(password) // Convertimos la password en un sha256
+                password: passHashed // Convertimos la password en un bcrypt
             }
         });
 
@@ -35,9 +43,11 @@ async function authRegister(email: string, password: string, userId: number) {
 async function authLogIn(email: string, password: string) {
     try{
         // Hacemos un findOne donde buscamos el email recibido y el password en sha256 en la tabla Auth
-        const findAuth = await Auth.findOne({where: {email, password: getSHA256ofString(password)}});
+        const findAuth = await Auth.findOne({where: {email}});
 
-        if(findAuth){ // Si encontro el user
+        const findAuthPassword = await findAuth.get('password') as string;
+
+        if(await bcrypt.compare(password, findAuthPassword)){ // Si encontro el user
             // Creamos un token con jwt guardando el id
             const token = jwt.sign({id: findAuth.get('userId')}, SECRET);
             return token; // Y retornamos el token
