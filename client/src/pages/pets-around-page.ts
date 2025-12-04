36ef1e2d11b76b3c6ea8739eaf575526aec44b97
Santiago Guzman <guzmansantiago833@gmail.com>
2025-12-03 23:49:57 -0500

    feat: add new features

--- client/src/pages/pets-around-page.ts
+++ client/src/pages/pets-around-page.ts
@@ -1,4 +1,5 @@
 import { state } from "../state";
+import { getLocationFromCoords } from "../lib/location";
 
 class PetsAroundPage extends HTMLElement {
     shadow: ShadowRoot;
@@ -26,16 +27,35 @@ class PetsAroundPage extends HTMLElement {
 
     async getPets(lat: number, lng: number){
         const response = await fetch(`http://localhost:3000/pets/around?lat=${lat}&lng=${lng}`);
-
         const data = await response.json();
 
-        return state.setState(data);
+        const petsWithLocation = await Promise.all(
+            data.pets.map(async (pet: any) => {
+                const textLocation = await getLocationFromCoords(pet._geoloc.lng, pet._geoloc.lat);
+
+                return {
+                    ...pet, // Copia name, imgUrl, _geoloc, id, etc. automáticamente
+                    textLocation // Agrega la propiedad nueva
+                }
+            })
+        )
+
+        return state.setState({pets: petsWithLocation});
     }
 
     render(){
         this.shadow.innerHTML = ''
 
         const section = document.createElement('section');
+
+        section.innerHTML = `
+            <h3>Mascotas perdidas cerca</h3>
+        `
+
+        const petCardsContainer = document.createElement('div');
+        petCardsContainer.classList.add('pets-card__container');
+
+        section.appendChild(petCardsContainer)
         const pets = state.getState().pets;
 
         pets.map((pet: any) => {
@@ -43,21 +63,92 @@ class PetsAroundPage extends HTMLElement {
             card.classList.add('pet-card');
 
             card.innerHTML = `
-                <img src="${pet.imgUrl}">
+                <img src="https://voca-land.sgp1.cdn.digitaloceanspaces.com/43844/1654148724932/a782407639e0a9fa0e1509391a3feb39414681de6795983c8e0d7c13670aa6f6.jpg">
                 <div class="pet-card__info">
                     <h3>${pet.name}</h3>
-                    <p>UBICACION</p>
+                    <p>${pet.textLocation}</p>
                     <a href="#">Reportar</a>
                 </div>
             `
 
-            section.appendChild(card);
+            petCardsContainer.appendChild(card);
         });
 
         const style = document.createElement('style');
 
         style.innerHTML = `
-        
+            section{
+                font-family: "Poppins", sans-serif;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                width: 90%;
+                margin: 0 auto;
+            }
+
+            section h3{
+                font-size: 24px;
+            }
+
+            .pets-card__container{
+                width: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 2rem;
+                flex-wrap: wrap;
+            }
+
+            .pet-card{
+                background-color: #26302E;
+                color: white;
+                border-radius: 10px;
+                padding: 8px;
+                width: 100%;
+                box-sizing: border-box;
+                max-width: 350px;
+                max-height: 234px;
+            }
+
+            .pet-card img{
+                width: 100%;
+                height: 136px;
+                object-fit: cover;
+                border-radius: 3px;
+            }
+
+            .pet-card__info{
+                display: grid;
+                align-items: center;
+                grid-template-columns: 60% 40%;
+                grid-template-rows: 50% 50%;
+            }
+
+            .pet-card__info *{
+                margin: 0;
+            }
+
+            .pet-card__info h3{
+                font-size: 36px;
+                margin-left: 8px;
+            }
+
+            .pet-card__info p{
+                grid-colum: 1;
+                grid-row: 2;
+                margin-left: 8px;
+            }
+
+            .pet-card__info a{
+                grid-colum: 2;
+                grid-row: span 2;
+                justify-self: center;
+                background-color: #EB6372;
+                padding: 10px 15px;
+                text-decoration: none;
+                color: white;
+                border-radius: 4px;
+            }
         `
 
         this.shadow.appendChild(section);
--- client/src/state.ts
+++ client/src/state.ts
@@ -4,9 +4,105 @@ const state = {
 		// Guardamos la data del estado en data
 		auth: "" as string,
 		me: "" as string,
-		pets: [] as any,
+		pets: [
+			{
+            "name": "Rocky",
+            "lost": true,
+            "lat": 4.655,
+            "lng": -74.095,
+            "imgUrl": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==",
+            "_geoloc": {
+                "lat": 4.655,
+                "lng": -74.095
+            },
+            "objectID": "rocky_manual_test",
+            "_highlightResult": {
+                "name": {
+                    "value": "Rocky",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "lat": {
+                    "value": "4.655",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "lng": {
+                    "value": "-74.095",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "imgUrl": {
+                    "value": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "_geoloc": {
+                    "lat": {
+                        "value": "4.655",
+                        "matchLevel": "none",
+                        "matchedWords": []
+                    },
+                    "lng": {
+                        "value": "-74.095",
+                        "matchLevel": "none",
+                        "matchedWords": []
+                    }
+                }
+            }
+        },
+        {
+            "name": "Luna",
+            "lost": true,
+            "lat": 4.71,
+            "lng": -74.07,
+            "imgUrl": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==",
+            "_geoloc": {
+                "lat": 4.71,
+                "lng": -74.07
+            },
+            "objectID": "luna_manual_test",
+            "_highlightResult": {
+                "name": {
+                    "value": "Luna",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "lat": {
+                    "value": "4.71",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "lng": {
+                    "value": "-74.07",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "imgUrl": {
+                    "value": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==",
+                    "matchLevel": "none",
+                    "matchedWords": []
+                },
+                "_geoloc": {
+                    "lat": {
+                        "value": "4.71",
+                        "matchLevel": "none",
+                        "matchedWords": []
+                    },
+                    "lng": {
+                        "value": "-74.07",
+                        "matchLevel": "none",
+                        "matchedWords": []
+                    }
+                }
+            }
+        }
+		] as any,
 		token: null as any,
-		coords: null as any
+		coords: {
+			lat: 4.65,
+			lng: -74.09
+		}
 	},
 	listeners: [] as any[], // Guarda el array de callbacks (listeners)
 	getState() {
