be16b4e6339fe2cb8b3cbb8a030edc23126fc934
Santiago Guzman <guzmansantiago833@gmail.com>
2025-12-05 18:44:03 -0500

    feat: frontend

--- client/src/index.ts
+++ client/src/index.ts
@@ -4,11 +4,20 @@ import { state } from "./state";
 // Creamos una funcion que se ejecuta al iniciar la aplicacion
 (()=>{
     // Recupera el estado del localStorage
-    const currentLocalStorage = localStorage.getItem("state");
+    const currentLocalStorage = localStorage.getItem("petFinderState");
 
     // Si encontro un estado
     if (currentLocalStorage) {
-        state.setState(JSON.parse(currentLocalStorage)); // Lo setea en el estado volatil
+        const localStorageParsed = JSON.parse(currentLocalStorage);
+        const dateNowAndSaved = Date.now() -localStorageParsed.savedAt;
+        if(dateNowAndSaved > 21600000){
+            state.setState({
+                token: localStorageParsed.token,
+                coords: null 
+            }); // Pero aqui eliminando la propiedad savedAt y coords, la de token si la podemos dejar
+        } else {
+            state.setState(localStorageParsed); // Lo setea en el estado volatil
+        }
     }
 
     return; // Y termina la funcion
@@ -20,11 +29,13 @@ state.suscribe(() => {
 	const currentState = state.getState();
 
     //Verifica que la propiedad token no este vacia
-	if (currentState.token) {
-		localStorage.setItem( // Y setea el token en el localStorage
-			"state",
-			JSON.stringify({ token: currentState.token })
-		);
+	if (currentState.token || currentState.coords) {
+		const payload = {
+            token: currentState.token,
+            coords: currentState.coords,
+            savedAt: Date.now()
+        };
+        localStorage.setItem("petFinderState", JSON.stringify(payload));
 		return;
 	}
 });
