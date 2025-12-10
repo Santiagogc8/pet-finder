1d0292d639cfbcfb95f514c1d830363d22ec753a
Santiago Guzman <guzmansantiago833@gmail.com>
2025-12-09 20:32:34 -0500

    feat: frontend

--- client/src/pages/update-profile.ts
+++ client/src/pages/update-profile.ts
@@ -11,18 +11,11 @@ class updateProfile extends HTMLElement {
     connectedCallback(){
         const currentState = state.getState();
 
-        if(currentState.token){
-            state.suscribe(() => { // Le pasamos una arrow function que ejecuta a render. De esta manera la toma, la guárdala, y  la ejecútala cuando el estado cambie
-                this.render();
-            });
-        } else {
-            history.pushState({}, "", "/");
-			window.dispatchEvent(new PopStateEvent("popstate")); // Le decimos a la ventana que la ruta cambio
-        }
+        this.render();
     }
 
     async sendNewProfileData(data: object){
-        
+
     }
 
     render(){
@@ -30,6 +23,20 @@ class updateProfile extends HTMLElement {
 
         const section = document.createElement('section');
         section.innerHTML = `
+            <h4>Datos personales</h4>
+            <form>
+                <div>
+                    <div class="form__inputs">
+                        <label for="name">Nombre</label>
+                        <input id="name" name="name" type="text">
+                    </div>
+                    <div class="form__inputs">
+                        <label for="location">Ubicacion</label>
+                        <input id="location" name="location" type="text">
+                    </div>
+                    </div>
+                <button type="submit">Guardar</button>
+            </form>
         `
 
         const style = document.createElement('style');
@@ -47,6 +54,48 @@ class updateProfile extends HTMLElement {
             section h4{
                 font-size: 36px;
                 margin: 50px 0;
+                width: 300px;
+                text-align: center;
+            }
+
+            form{
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                width: 100%;
+            }
+
+            .form__inputs{
+                display: flex;
+                flex-direction: column;
+                margin-bottom: 25px;
+            }
+
+            .form__inputs label{
+                text-transform: uppercase;
+            }
+
+            .form__inputs input{
+                padding: 16px 8px;
+                width: 100%;
+                box-sizing: border-box;
+                border: none;
+                border-radius: 4px;
+                font-size: 15px;
+                font-family: "Poppins", sans-serif;
+            }
+
+            form button{
+                margin-top: 2vh;
+                padding: 16px 8px;
+                width: 100%;
+                background-color: #5A8FEC;
+                color: white;
+                border: none;
+                border-radius: 4px;
+                font-size: 1rem;
+                font-weight: 600;
             }
         `
 
