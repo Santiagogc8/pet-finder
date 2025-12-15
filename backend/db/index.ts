dfb7c2bda72b5405862fd5e3a6e9a02d4ffd2857
Santiago Guzman <guzmansantiago833@gmail.com>
2025-12-15 16:23:19 -0500

    feat: add new features

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,4 +1,4 @@
 import { Sequelize } from "sequelize"; // Importamos sequelize
 
 // Inicializamos el objeto Sequelize y le pasamos el link de la conexion
-export const sequelize = new Sequelize('postgresql://neondb_owner:npg_tBqJrZ9Vng8I@ep-autumn-mode-ahlwgasj-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+export const sequelize = new Sequelize(process.env.SEQUELIZE);
